9614ad7f18449d0f6f3c21471ade3e8fdb4aca01
Ivan Bespalov <slate245@gmail.com>
2020-01-30 16:31:31 +0500

    Added DatePicker Component
    Also added Material UI theme (in a rudimentary form)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { CssBaseline, Paper } from "@material-ui/core";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import green from "@material-ui/core/colors/green";
+import { MuiPickersUtilsProvider } from "@material-ui/pickers";
 import { Switch, Route, Redirect } from "react-router-dom";
-import "./App.css";
+import DateFnsUtils from "@date-io/date-fns";
+import ruLocale from "date-fns/locale/ru";
 
 import Schedule from "./components/schedule";
 import Activities from "./components/activities";
@@ -9,19 +13,27 @@ import Settings from "./components/settings";
 import NavBar from "./components/navBar";
 
 function App() {
+  const theme = createMuiTheme({
+    palette: {
+      primary: green
+    }
+  });
+
   return (
-    <>
-      <CssBaseline />
-      <Paper square component="main" elevation={0}>
-        <Switch>
-          <Route path="/schedule" component={Schedule} />
-          <Route path="/activities" component={Activities} />
-          <Route path="/settings" component={Settings} />
-          <Redirect from="/" exact to="/schedule" />
-        </Switch>
-      </Paper>
-      <NavBar />
-    </>
+    <ThemeProvider theme={theme}>
+      <MuiPickersUtilsProvider utils={DateFnsUtils} locale={ruLocale}>
+        <CssBaseline />
+        <Paper square component="main" elevation={0}>
+          <Switch>
+            <Route path="/schedule" component={Schedule} />
+            <Route path="/activities" component={Activities} />
+            <Route path="/settings" component={Settings} />
+            <Redirect from="/" exact to="/schedule" />
+          </Switch>
+        </Paper>
+        <NavBar />
+      </MuiPickersUtilsProvider>
+    </ThemeProvider>
   );
 }
 
--- src/components/schedule.jsx
+++ src/components/schedule.jsx
@@ -1,6 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
+import { DatePicker } from "@material-ui/pickers";
 import Timetable from "./timetable";
 
+const useStyles = makeStyles({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    paddingTop: "16px"
+  }
+});
+
 export default function Schedule() {
-  return <Timetable />;
+  const classes = useStyles();
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  return (
+    <section className={classes.root}>
+      <DatePicker
+        label="Сегодня"
+        value={selectedDate}
+        onChange={setSelectedDate}
+        inputVariant="outlined"
+        format="EEEEEE, d MMMM"
+      />
+      <Timetable />
+    </section>
+  );
 }
