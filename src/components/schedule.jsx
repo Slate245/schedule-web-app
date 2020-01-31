ca188e498423b4bdb21f4249632900f198ef256a
Ivan Bespalov <slate245@gmail.com>
2020-01-31 11:05:20 +0500

    Added FAB Component

--- src/components/schedule.jsx
+++ src/components/schedule.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 
+import { Fab } from "@material-ui/core";
+import { Add } from "@material-ui/icons";
 import { DatePicker } from "@material-ui/pickers";
 import Timetable from "./timetable";
 
@@ -10,6 +12,15 @@ const useStyles = makeStyles({
     flexDirection: "column",
     alignItems: "center",
     paddingTop: "16px"
+  },
+  fab: {
+    backgroundColor: "white",
+    color: "#4CAF50",
+    position: "fixed",
+    bottom: "56px",
+    right: "0",
+    margin: "0 16px 16px 0",
+    alignSelf: "flex-end"
   }
 });
 
@@ -27,6 +38,9 @@ export default function Schedule() {
         format="EEEEEE, d MMMM"
       />
       <Timetable />
+      <Fab className={classes.fab}>
+        <Add />
+      </Fab>
     </section>
   );
 }
