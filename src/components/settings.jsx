d3b6351c9985355d0c70513816b295056bdc162a
Ivan Bespalov <slate245@gmail.com>
2020-03-29 16:29:14 +0500

    Added logout functionality to Settings component

--- src/components/settings.jsx
+++ src/components/settings.jsx
@@ -1,5 +1,41 @@
-import React from "react";
+import React, { useContext } from "react";
+import { logout } from "../services/authService";
+import { UserContext } from "../utils/userContext";
+import { Typography, Button, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  root: {
+    height: "calc(100vh - 56px)",
+    padding: "1rem"
+  },
+  caption: {
+    margin: "0 0 2rem"
+  }
+});
 
 export default function Settings() {
-  return <h1>Settings</h1>;
+  const { user } = useContext(UserContext);
+  const classes = useStyles();
+
+  function handleLogout() {
+    logout();
+    window.location = "/";
+  }
+  return (
+    <section className={classes.root}>
+      <Typography className={classes.header} variant="h6">
+        Настройки
+      </Typography>
+      <Typography
+        variant="body2"
+        color="textSecondary"
+        className={classes.caption}
+      >
+        Вы вошли в систему как {user.name}
+      </Typography>
+      <Button onClick={handleLogout} size="small" variant="outlined">
+        Выйти из системы
+      </Button>
+    </section>
+  );
 }
