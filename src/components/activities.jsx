71e39e764ff83a43b7297e2e87ebd9a864155c9a
Ivan Bespalov <slate245@gmail.com>
2020-04-07 20:37:13 +0500

    Setup initial Activities screen

--- src/components/activities.jsx
+++ src/components/activities.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  root: {
+    padding: "1rem",
+    minHeight: "calc(100vh - 56px)",
+  },
+});
 
 export default function Activities() {
-  return <h1>Activities</h1>;
+  const classes = useStyles();
+
+  return <section className={classes.root}>Activities</section>;
 }
