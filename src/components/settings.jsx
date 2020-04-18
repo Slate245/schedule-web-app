78b777d122939782bb856909d86edec8f7b02527
Ivan Bespalov <slate245@gmail.com>
2020-04-18 20:29:17 +0500

    Added bugreport link

--- src/components/settings.jsx
+++ src/components/settings.jsx
@@ -1,16 +1,21 @@
 import React, { useContext } from "react";
 import { logout } from "../services/authService";
 import { UserContext } from "../utils/userContext";
-import { Typography, Button, makeStyles } from "@material-ui/core";
+import { Typography, Button, Link, makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles({
   root: {
     height: "calc(100vh - 56px)",
-    padding: "1rem"
+    padding: "1rem",
+    position: "relative",
   },
   caption: {
-    margin: "0 0 2rem"
-  }
+    margin: "0 0 2rem",
+  },
+  bugreport: {
+    position: "absolute",
+    bottom: "1rem",
+  },
 });
 
 export default function Settings() {
@@ -36,6 +41,20 @@ export default function Settings() {
       <Button onClick={handleLogout} size="small" variant="outlined">
         Выйти из системы
       </Button>
+      <Typography
+        className={classes.bugreport}
+        variant="body2"
+        color="textSecondary"
+      >
+        Нашли проблему?{" "}
+        <Link
+          href="https://forms.gle/cje3FQ6E7SoyibE68"
+          target="_blank"
+          rel="noopener"
+        >
+          Расскажите мне!
+        </Link>
+      </Typography>
     </section>
   );
 }
